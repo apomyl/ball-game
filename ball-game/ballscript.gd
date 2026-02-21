8695b554b1dbe0535988876060ffee95d927dd9c
jin <in24429@bristol.ac.uk>
2026-02-21 15:06:10 +0000

    ball script

--- ball-game/ballscript.gd
+++ ball-game/ballscript.gd
@@ -1,11 +1,21 @@
-extends Node
+extends RigidBody2D
 
+var planet_name = "Uranus"
+var planet_speed = 400.0
+var planet_health = 100.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	var random_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	linear_velocity = random_direction * planet_speed
+	body_entered.connect(_on_body_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body):    
+	if body is RigidBody2D:
+		var damage_taken = body.mass * 10 
+		planet_health -= damage_taken
+		
+		print(planet_name + " was hit! Health: " + str(planet_health))
+		
+		if planet_health <= 0:
+			print(planet_name + " was destroyed!")
+			queue_free()
