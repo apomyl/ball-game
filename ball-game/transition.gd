50d06d7bcc1c2203394dbe3eaaab00504d2c2c8d
susangkyparamesvaran <hi24376@bristol.ac.uk>
2026-02-22 09:37:28 +0000

    mkjufjhhgjg

--- ball-game/transition.gd
+++ ball-game/transition.gd
@@ -5,5 +5,11 @@ func _ready():
 	var texture_size = sprite_frames.get_frame_texture(animation, 0).get_size()
 	
 	scale = screen_size / texture_size
+	
+	animation_finished.connect(_on_animation_finished)
 
 	play("run")
+
+func _on_animation_finished():
+	get_tree().change_scene_to_file("res://ui/battles/battle.tscn")
+	
--- ball-game/ui/profiles/pluto_battle.gd
+++ ball-game/ui/profiles/pluto_battle.gd
@@ -3,4 +3,4 @@ extends Button
 func _on_pressed():
 
 	PlayerDetails.e_path = "res://planets/pluto.tscn"
-	get_tree().change_scene_to_file("res://ui/battles/battle.tscn")
+	get_tree().change_scene_to_file("res://ui/transition.tscn")
